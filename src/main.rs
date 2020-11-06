48a07d0d6f2d2c47ec1b05a25935708cc06a5af3
Antoine Gagné <gagnantoine@gmail.com>
2020-11-05 21:34:32 -0500

    Add initial setup

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+extern crate error_chain;
+extern crate glob;
+
+use error_chain::error_chain;
+use structopt::StructOpt;
+use structopt::clap::{ AppSettings };
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    about = "Create gitignore",
+    settings = &[
+        AppSettings::ColoredHelp,
+        AppSettings::SubcommandRequiredElseHelp,
+        AppSettings::UnifiedHelpMessage,
+        AppSettings::VersionlessSubcommands
+    ])]
+enum Options {
+    #[structopt(about = "Outputs the gitignore",
+                settings = &[AppSettings::ColoredHelp, AppSettings::UnifiedHelpMessage])]
+    Create {
+        #[structopt(name = "TARGETS", help = "Specifies the tools/languages", required = true)]
+        targets: Vec<String>
+    },
+
+    #[structopt(about = "Lists the available tools and languages",
+                settings = &[AppSettings::ColoredHelp, AppSettings::UnifiedHelpMessage])]
+    List
+}
+
+error_chain! {
+    foreign_links {
+        HttpRequest(reqwest::Error);
+    }
+}
+
+const URL: &str = "https://www.gitignore.io/api/";
+const LISTING_PATH: &str = "list";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let matches = Options::from_args();
+
+    let path = match matches {
+        Options::List =>
+            LISTING_PATH.to_owned(),
+        Options::Create { targets } =>
+            targets.join(",")
+
+    };
+    let full_url = URL.to_owned() + &path;
+    let response = reqwest::get(&full_url).await?;
+    let body = response.text().await?;
+    println!("{}", body);
+    Ok(())
+}
