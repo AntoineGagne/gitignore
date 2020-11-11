a2e9c53519aaa054435d4b5c429a6a024524b9bd
Antoine Gagné <gagnantoine@gmail.com>
2020-11-10 22:14:25 -0500

    Format code

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate error_chain;
 extern crate glob;
 
 use error_chain::error_chain;
+use structopt::clap::AppSettings;
 use structopt::StructOpt;
-use structopt::clap::{ AppSettings };
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -18,13 +18,17 @@ enum Options {
     #[structopt(about = "Outputs the gitignore",
                 settings = &[AppSettings::ColoredHelp, AppSettings::UnifiedHelpMessage])]
     Create {
-        #[structopt(name = "TARGETS", help = "Specifies the tools/languages", required = true)]
-        targets: Vec<String>
+        #[structopt(
+            name = "TARGETS",
+            help = "Specifies the tools/languages",
+            required = true
+        )]
+        targets: Vec<String>,
     },
 
     #[structopt(about = "Lists the available tools and languages",
                 settings = &[AppSettings::ColoredHelp, AppSettings::UnifiedHelpMessage])]
-    List
+    List,
 }
 
 error_chain! {
@@ -41,11 +45,8 @@ async fn main() -> Result<()> {
     let matches = Options::from_args();
 
     let path = match matches {
-        Options::List =>
-            LISTING_PATH.to_owned(),
-        Options::Create { targets } =>
-            targets.join(",")
-
+        Options::List => LISTING_PATH.to_owned(),
+        Options::Create { targets } => targets.join(","),
     };
     let full_url = URL.to_owned() + &path;
     let response = reqwest::get(&full_url).await?;
